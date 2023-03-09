6d47c35f99125d909b2e055d022e68c56d830320
AnangNaz <anangnaz@gmail.com>
2023-03-09 09:33:48 +0700

    membuat website quiz

---
+++ js/que.js
@@ -0,0 +1,60 @@
+//creating an array and passing the number, questions, options, and answer
+
+let questions = [
+    {
+        numb: 1,
+        question: "A country with an icon of a lion head statue that spouts water?",
+        answer: "Singapore",
+        options: [
+            "Singapore",
+            "Japan",
+            "South Korea",
+            "North Korea"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "The country with the nickname of the rising sun is?",
+        answer: "Japan",
+        options: [
+            "Singapore",
+            "Japan",
+            "South Korea",
+            "North Korea"
+        ]
+    },
+    {
+        numb: 3,
+        question: "A country with the nickname of the hell on earth?",
+        answer: "Kamboja",
+        options: [
+            "Italy",
+            "Germany",
+            "Kamboja",
+            "Myanmar"
+        ]
+    },
+    {
+        numb: 4,
+        question: "A country with the nickname Equatorial Emerald is?",
+        answer: "Indonesia",
+        options: [
+            "Malaysia",
+            "South Africa",
+            "Laos",
+            "Indonesia"
+        ]
+    },
+    {
+        numb: 5,
+        question: "During the Miracle in Istanbul match which football club is competing?",
+        answer: "Liverpool Vs Ac Milan",
+        options: [
+            "Manchester United Vs Barcelona",
+            "Chelsea Vs Arsenal",
+            "Liverpool Vs Ac Milan",
+            "Manchester United Vs Liverpool"
+        ]
+    } 
+];
